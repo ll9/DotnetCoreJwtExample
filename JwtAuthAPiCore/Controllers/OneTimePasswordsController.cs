00ba4d8779e45f101b9fe542a92d22b481a02c43
ll9 <llwierer@web.de>
2019-02-27 20:42:31 +0100

    Generating one time password

---
+++ JwtAuthAPiCore/Controllers/OneTimePasswordsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using JwtAuthAPiCore.Data;
+using JwtAuthAPiCore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Security.Claims;
+
+namespace JwtAuthAPiCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OneTimePasswordsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OneTimePasswordsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/OneTimePasswords
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPost]
+        public IActionResult GetOneTimePassword([FromBody] MobileUser mobileUser)
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            string password;
+
+            do
+            {
+                password = OneTimePassword.GeneratePassword();
+
+            } while (_context.OneTimePasswords.Any(p => p.Password.Equals(password)));
+
+            var oneTimePassword = new OneTimePassword(password, mobileUser);
+            _context.OneTimePasswords.Add(oneTimePassword);
+
+            return Ok(oneTimePassword.Password);
+        }
+
+        private bool OneTimePasswordExists(string id)
+        {
+            return _context.OneTimePasswords.Any(e => e.Id == id);
+        }
+    }
+}
--- JwtAuthAPiCore/Models/OneTimePassword.cs
+++ JwtAuthAPiCore/Models/OneTimePassword.cs
@@ -2,19 +2,50 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace JwtAuthAPiCore.Models
 {
     public class OneTimePassword
     {
+        public OneTimePassword()
+        {
+
+        }
+
+        public OneTimePassword(string password, MobileUser mobileUser)
+        {
+            Password = password;
+            MobileUser = mobileUser;
+        }
+
         [Key]
-        public string Id { get; set; }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Password { get; set; }
-        public DateTime Expires { get; set; }
-        public bool IsConsumed { get; set; }
+        public DateTime Expires { get; set; } = DateTime.Now.AddMinutes(5);
+        public bool IsConsumed { get; set; } = false;
 
         public string MobileUserId { get; set; }
         public MobileUser MobileUser { get; set; }
+
+        public static string GeneratePassword(int length = 8, int step = 4)
+        {
+            var random = new Random();
+            var chars = "ABCDEFGHJKMNPQRSTUVWXYZ23456789";
+            var passwordBuilder = new StringBuilder();
+
+            for (int i = 0; i < length; i++)
+            {
+                if (i != 0 && i % step == 0)
+                {
+                    passwordBuilder.Append("-");
+                }
+
+                passwordBuilder.Append(chars[random.Next(chars.Length)]);
+            }
+
+            return passwordBuilder.ToString();
+        }
     }
 }
